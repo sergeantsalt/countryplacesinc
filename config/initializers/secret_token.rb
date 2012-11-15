492b78b0229f5781eb74226398ab7ad3e9578900
Alex Nelson <sergeantsalt@gmail.com>
2012-11-14 21:40:01 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Countryplacesinc::Application.config.secret_token = '649b7b628fbd8597c8ed9229f364134776eee6274508adf837cb336cd767422416cf0e1a2ffd8087129c8e2b897f77f61c1472bbd798840116e0d5d5347c61f3'
